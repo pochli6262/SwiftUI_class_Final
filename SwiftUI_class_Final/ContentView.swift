a0b03b30af0aa20f5b4bb688e1d5cd57401f88bb
jason <pochli6262@gmail.com>
2025-05-09 10:26:00 +0800

    Initial Commit

---
+++ SwiftUI_class_Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_class_Final
+//
+//  Created by 廖柏鈞 on 2025/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_class_Final/SwiftUI_class_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_class_FinalApp.swift
+//  SwiftUI_class_Final
+//
+//  Created by 廖柏鈞 on 2025/5/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_class_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
